19fe446a81be2ed28ba139b160bb8cb2a60a768b
Stefani Kostic <stefanikostic6@gmail.com>
2025-06-11 13:41:11 +0200

    Renamed PreviewResumeServiceImplApplicationTests

--- src/test/java/com/evelateresume/preview_resume_service/PreviewResumeServiceImplApplicationTests.java
+++ src/test/java/com/evelateresume/preview_resume_service/PreviewResumeServiceImplApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class PreviewResumeServiceApplicationTests {
+class PreviewResumeServiceImplApplicationTests {
 
 	@Test
 	void contextLoads() {
